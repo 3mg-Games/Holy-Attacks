faeb98199b72d22e27dc5ea2ea66c8d3c8e2367a
Satvik Tiwari <satviktiwarikivtas7@gmail.com>
2021-10-20 21:14:07 +0530

    Added magic attack

--- Holy Attacks/Assets/PlayerMovement.cs
+++ Holy Attacks/Assets/PlayerMovement.cs
@@ -6,17 +6,26 @@ public class PlayerMovement : MonoBehaviour
 {
   //  [SerializeField] CharacterController controller;
     [SerializeField] float movementSpeed = 10f;
+    [SerializeField] SpriteRenderer playerRadius;
+
+    [SerializeField] GameObject majicShoo;
+    [SerializeField] float projectileForce = 10f;
+    [SerializeField] Transform projectilePos;
+
+    [SerializeField] float touchSensitivity = 0.3f;
     //[SerializeField] float rotationSpeed = 1.0f;
    // [SerializeField] float rotationStep = 10f;
 
     Joystick joystick;
     float x, z;
     float singleStep;
+    bool isPlayerMoving = false;
     // Start is called before the first frame update
     void Start()
     {
         joystick = FindObjectOfType<Joystick>();
     }
+
 
     // Update is called once per frame
     void Update()
@@ -41,8 +50,33 @@ public class PlayerMovement : MonoBehaviour
         transform.rotation = Quaternion.LookRotation(newDir);
         */
 
-        float moveHorizontal = joystick.Horizontal;
-        float moveVertical = joystick.Vertical;
+        
+
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        if (Input.touchCount > 0)
+        {
+            moveHorizontal = Input.touches[0].deltaPosition.x;
+            moveVertical = Input.touches[0].deltaPosition.y;
+
+            moveHorizontal *= touchSensitivity;
+            moveVertical *= touchSensitivity;
+        }
+
+        if (moveHorizontal == 0 && moveVertical == 0)
+        {
+            isPlayerMoving = false;
+            playerRadius.enabled = true;
+        }
+
+        else
+        {
+            isPlayerMoving = true;
+            playerRadius.enabled = false;
+        }
+
+        
 
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
         transform.rotation = Quaternion.LookRotation(movement);
@@ -50,4 +84,18 @@ public class PlayerMovement : MonoBehaviour
 
         transform.Translate(movement * movementSpeed * Time.deltaTime, Space.World);
     }
+
+    public bool GetIsPlayerMoving()
+    {
+        return isPlayerMoving;
+    }
+
+    public void AttackEnemy(Transform enemy)
+    {
+        GameObject magicAttack = Instantiate(majicShoo, projectilePos.position, Quaternion.identity);
+        Vector3 dir = enemy.position - magicAttack.transform.position;
+        magicAttack.GetComponent<Rigidbody>().AddForce(dir.normalized * projectileForce, ForceMode.Impulse);
+    }
+
+    
 }
